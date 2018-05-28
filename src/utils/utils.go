3dd56958daf60be941bbf82f56573a8235e09795
月杪 <moonkinstar@gmail.com>
2018-05-28 13:05:47 +0800

    添加utils.go

---
+++ src/utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"os"
+	"fmt"
+	"errors"
+)
+
+func CheckoutDir(dir string) error {
+	if _, err := os.Stat(dir); err == nil {
+
+	} else {
+		fmt.Println("Dir not exists, try to create...", dir)
+		err := os.MkdirAll(dir, 0711)
+		if err != nil {
+			fmt.Println("Error creating directory", dir)
+			fmt.Println("err:", err)
+			return errors.New(string("Error creating directory"))
+		}
+	}
+	return nil
+}
