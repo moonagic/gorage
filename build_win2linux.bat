ddcfc683c9542092dc1cbf54b0b6d186e074ebb0
月杪 <moonkinstar@gmail.com>
2018-06-11 11:24:36 +0800

    Update build_win2linux.bat

--- build_win2linux.bat
+++ build_win2linux.bat
@@ -1,5 +1,6 @@
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o gorage src/main.go
+go build -o gorage-linux-amd64 src/main.go
+tar cfJ gorage-linux-amd64.tar.xz gorage-linux-amd64
 
