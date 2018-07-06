9d44a2a5dbc1e13342ae6e9e682d15f68f7c0d96
月杪 <moonkinstar@gmail.com>
2018-07-06 11:05:51 +0800

    Update build_mac2linux.sh

--- build_mac2linux.sh
+++ build_mac2linux.sh
@@ -1,2 +1,4 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gorage-linux-amd64 src/main.go
-tar cfJ gorage-linux-amd64.tar.xz gorage-linux-amd64
+mkdir gorage-linux-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gorage-linux-amd64/gorage-linux-amd64 src/main.go
+tar cfJ gorage-linux-amd64.tar.xz gorage-linux-amd64/gorage-linux-amd64
+rm -rf gorage-linux-amd64/
